bce4243cb7f07cfd77f3915dc08bedc017acca2d
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2013-01-23 08:13:30 +0530

    added dynamic header

--- lib/export_csv/base.rb
+++ lib/export_csv/base.rb
@@ -13,16 +13,24 @@ module ExportCsv
     def export_to_csv(filename, model, escape_att ,options = {})
       csv_options = {}
       csv_options[:col_sep] = options[:separator] || ','
+
+      headers = options[:headers]
+      csv_options[:headers] = headers.present?
+
       model_dir = Dir['**/models/**/*.rb'].detect {|f| model == File.basename(f, '.*').camelize}
       if !model_dir.eql?(nil)
         table = File.basename(model_dir, '.*').camelize.constantize
         objects = table.all
         data = CSV.generate(csv_options) do |csv|
-          table.column_names.delete_if {|k, v|  escape_att.include? k }
-          csv << table.column_names.map {|k| k.gsub(/_/, ' ').capitalize}
+          if csv_options[:headers]
+            csv << headers
+          else
+            headers = table.column_names.delete_if {|k, v|  escape_att.include? k }
+            csv << headers.map {|k| k.gsub(/_/, ' ').capitalize}
+          end
           row = Array.new
           objects.each do |obj|
-            table.column_names.each do |col|
+            headers.each do |col|
               row << obj[col]
             end
             csv << row
