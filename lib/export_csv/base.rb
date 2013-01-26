f27228c10c097f8d906deeaf49a44daa8652618a
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2013-01-26 11:08:37 +0530

    added resque

--- lib/export_csv/base.rb
+++ lib/export_csv/base.rb
@@ -17,28 +17,42 @@ module ExportCsv
       headers = options[:headers]
       csv_options[:headers] = headers.present?
 
-      model_dir = Dir['**/models/**/*.rb'].detect {|f| model == File.basename(f, '.*').camelize}
-      if !model_dir.eql?(nil)
-        table = File.basename(model_dir, '.*').camelize.constantize
-        objects = table.all
-        data = CSV.generate(csv_options) do |csv|
-          if csv_options[:headers]
-            csv << headers
-          else
-            headers = table.column_names.delete_if {|k, v|  escape_att.include? k }
-            csv << headers.map {|k| k.gsub(/_/, ' ').capitalize}
-          end
-          row = Array.new
-          objects.each do |obj|
-            headers.each do |col|
-              row << obj[col]
+      begin
+        model_dir = Dir['**/models/**/*.rb'].detect {|f| model == File.basename(f, '.*').camelize}
+        if !model_dir.eql?(nil)
+          table = File.basename(model_dir, '.*').camelize.constantize
+          objects = table.all
+          begin
+            data = CSV.generate(csv_options) do |csv|
+              if csv_options[:headers]
+                csv << headers
+              else
+                headers = table.column_names.delete_if {|k, v|  escape_att.include? k }
+                csv << headers.map {|k| k.gsub(/_/, ' ').capitalize}
+              end
+              row = Array.new
+              objects.each do |obj|
+                headers.each do |col|
+                  begin
+                    row << obj[col]
+                  rescue Exception => e
+                    row << 'Invalid data field'
+                  end
+                end
+                csv << row
+                row.clear
+              end
             end
-            csv << row
-            row.clear
+          rescue SystemCallError
+            $stderr.print "IO failed: " + $!
+            raise
           end
+        else
+          puts "Table #{ENV['model']} could not be found"
         end
-      else
-        puts "Table #{ENV['model']} could not be found"
+      rescue SystemCallError
+        $stderr.print "IO failed: " + $!
+        raise
       end
 
       send_data(data, :filename => filename,:type => 'text/csv')
